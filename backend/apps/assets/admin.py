cb2fdb4ba23bbf8b3451ccde5166b3e81da0373b
LucasCanella99 <lucascanella19@gmail.com>
2026-06-05 14:24:36 -0300

    Fix de admin 2

--- backend/apps/assets/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import SiteAsset
-
-@admin.register(SiteAsset)
-class SiteAssetAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_at') # Esto es para que se vean columnas en el admin
-    search_fields = ('name',)
--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from apps.users.models import User
 from django.contrib.auth.models import Group
+
 # Register your models here.
 
 admin.site.register(User)
@@ -8,4 +9,14 @@ admin.site.register(User)
 try:
     admin.site.unregister(Group)
 except admin.sites.NotRegistered:
+    pass
+
+try:
+    from axes.models import AccessAttempt, AccessLog, AccessFailure
+    
+   
+    admin.site.unregister(AccessAttempt)
+    admin.site.unregister(AccessLog)
+    admin.site.unregister(AccessFailure)
+except (ImportError, admin.sites.NotRegistered):
     pass
