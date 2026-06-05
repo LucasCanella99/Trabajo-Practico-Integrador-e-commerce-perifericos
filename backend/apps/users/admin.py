bb39626180844e60095e99fc7777d65e12980d95
LucasCanella99 <lucascanella19@gmail.com>
2026-06-05 14:17:12 -0300

    Fix de admin

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from apps.users.models import User
+from django.contrib.auth.models import Group
 # Register your models here.
 
 admin.site.register(User)
+
+try:
+    admin.site.unregister(Group)
+except admin.sites.NotRegistered:
+    pass
